4944a1a7a821f9285f6ee61ae025175d9bb4f051
さいかい <70578583+saikai-moviestudio@users.noreply.github.com>
2021-08-16 19:04:56 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+const config = {
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
+};
+
+module.exports = withPWA(config);
